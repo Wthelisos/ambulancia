b9699977701b6c801a18b43a56d2e898142dfdb7
wthelisos <wthelisos@gmail.com>
2021-12-13 13:15:02 +0100

    first commit

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+	'vehiclelayouts.meta',
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'dlctext.meta',
+}
+
+data_file 'DLC_TEXT_FILE' 'dlctext.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
